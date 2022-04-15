20ba1b11819c8c7c3c3cf480b97892a3b2e935b7
miguel <miglopes1904@gmail.com>
2022-04-15 22:07:12 +0100

    initial commit
    initial commit

---
+++ src/main/java/com/learningscorecard/ucs/UcsApplication.java
@@ -0,0 +1,13 @@
+package com.learningscorecard.ucs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningscorecard/ucs/UcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningscorecard.ucs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
