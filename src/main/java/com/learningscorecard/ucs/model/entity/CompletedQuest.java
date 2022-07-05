adcf96ff206f5ba37b8407f1256f3ec4724edaa8
miguel <miglopes1904@gmail.com>
2022-07-05 11:36:31 +0100

    add validation date to completed quest
    add validation date to completed quest

--- src/main/java/com/learningscorecard/ucs/model/entity/CompletedQuest.java
+++ src/main/java/com/learningscorecard/ucs/model/entity/CompletedQuest.java
@@ -1,5 +1,6 @@
 package com.learningscorecard.ucs.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import javax.persistence.Column;
@@ -28,13 +29,20 @@ public class CompletedQuest {
     private String title;
 
     @Column(name = "start_date")
+    @JsonProperty("start_date")
     private LocalDate startDate;
 
     @Column(name = "end_date")
+    @JsonProperty("end_date")
     private LocalDate endDate;
 
     @Column(name = "last_chance_date")
+    @JsonProperty("last_chance_date")
     private LocalDate lastChanceDate;
+
+    @Column(name = "validation_date")
+    @JsonProperty("validation_date")
+    private LocalDate validationDate;
 
     @Builder.Default
     private LocalDate timestamp = LocalDate.now();
