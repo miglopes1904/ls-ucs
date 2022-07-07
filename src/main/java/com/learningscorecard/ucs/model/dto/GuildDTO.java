4cff7c62036658094c32cbdd4856269927ad32dd
miguel <miglopes1904@gmail.com>
2022-07-08 00:23:37 +0100

    update guild dto
    update guild dto

--- src/main/java/com/learningscorecard/ucs/model/dto/GuildDTO.java
+++ src/main/java/com/learningscorecard/ucs/model/dto/GuildDTO.java
@@ -1,11 +1,13 @@
 package com.learningscorecard.ucs.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.learningscorecard.ucs.model.entity.Feedback;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.ElementCollection;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +30,9 @@ public class GuildDTO {
     private LocalDate createdAt;
 
     private String alliance;
+
+    @Builder.Default
+    private List<Feedback> feedbacks = new ArrayList<>();
 
     @JsonProperty("project_description")
     private String projectDescription;
